9da69b15b2c38efc36e730111ac4a8322770e4f6
Alexandre Morey <alexander006_9@hotmail.com>
2021-11-04 10:19:34 +0000

    header set up

--- src/App.js
+++ src/App.js
@@ -2,14 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-const name = 'Alex'
-const x = true
-
   return (
     <div className="container">
       <h1>Hello From React</h1>
-      <h1>Hello {name}</h1>
-      <h1>Hello {x ? 'yes' : 'no'}</h1>
     </div>
   );
 }
