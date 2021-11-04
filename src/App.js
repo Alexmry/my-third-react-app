44da185ae7a2f665629c73cf5710c8f1063ede6f
Alexandre Morey <alexander006_9@hotmail.com>
2021-11-04 10:34:07 +0000

    header props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="container">
-      <h1>Hello From React</h1>
+      <h1>Hello From React2</h1>
+      <Header title='Hello' />
     </div>
   );
 }
