c3e020177e66f7bc14373f667f57f3200b39bdf4
Alexandre Morey <alexander006_9@hotmail.com>
2021-11-10 11:23:40 +0000

    tasks unique key prop

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,30 +1,32 @@
 const tasks = [
-    {
-        id: 1,
-        text: 'Doctor Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
-    },
-    {
-        id: 2,
-        text: 'Meeting At School',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true,
-    },
-    {
-        id: 3,
-        text: 'Food Shoping',
-        day: 'Feb 5th at 4:30pm',
-        reminder: false,
-    },
-]
+  {
+    id: 1,
+    text: "Doctor Appointment",
+    day: "Feb 5th at 2:30pm",
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: "Meeting At School",
+    day: "Feb 6th at 1:30pm",
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: "Food Shoping",
+    day: "Feb 5th at 4:30pm",
+    reminder: false,
+  },
+];
 
 const Tasks = () => {
-    return (
-        <>
-            {tasks.map((task) => (<h3>{task.text}</h3>))}
-        </>
-    )
-}
+  return (
+    <>
+      {tasks.map((task) => (
+        <h3 key={task.id}>{task.text}</h3>
+      ))}
+    </>
+  );
+};
 
-export default Tasks
+export default Tasks;
