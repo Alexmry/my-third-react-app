462062edc39665abbb3b63b3d4ab737015addefa
Alexandre Morey <alexander006_9@hotmail.com>
2021-11-11 15:39:37 +0000

    toggle fuction

--- src/App.js
+++ src/App.js
@@ -34,13 +34,22 @@ function App() {
   // Toggle Reminder
 
   const toggleReminder = (id) => {
-    console.log(id)
-  }
+    console.log(id);
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
 
   return (
     <div className="container">
       <Header />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Show'}
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Tasks To Show"
+      )}
     </div>
   );
 }
