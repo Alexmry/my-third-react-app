1fa6ca5f87988f69961f32cbdfa0c05358da3c29
Alexandre Morey <alexander006_9@hotmail.com>
2021-11-11 15:27:13 +0000

    toggle props

--- src/App.js
+++ src/App.js
@@ -31,10 +31,16 @@ function App() {
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
+  // Toggle Reminder
+
+  const toggleReminder = (id) => {
+    console.log(id)
+  }
+
   return (
     <div className="container">
       <Header />
-      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} /> : 'nothing'}
+      {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Show'}
     </div>
   );
 }
