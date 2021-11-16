510922b0c4cef54144c4bd5ea3ac3fcb6fdc36f6
Alexandre Morey <alexander006_9@hotmail.com>
2021-11-16 10:32:47 +0000

    get task from server

--- src/App.js
+++ src/App.js
@@ -8,15 +8,23 @@ function App() {
   const [tasks, setTasks] = useState([]);
 
   useEffect(() => {
-    const fetchTasks = async () => {
-      const res = await fetch('http://localhost:5000/tasks')
-      const data = await res.json()
-
-      console.log(data)
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
     }
-    fetchTasks()
+
+    // fetchTasks()
+    getTasks()
   }, [])
 
+  // Fetch Tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    // console.log(data)
+    return data
+  }
   // Add Task
   const addTask = (task) => {
     console.log(task)
