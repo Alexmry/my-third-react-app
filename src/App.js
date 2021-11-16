d95fda9f3e73623b78d2f7889d8cbc8802beff08
Alexandre Morey <alexander006_9@hotmail.com>
2021-11-16 10:26:24 +0000

    fetch task form json

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTasK from "./components/AddTask";
@@ -6,6 +6,16 @@ import AddTasK from "./components/AddTask";
 function App() {
   const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const res = await fetch('http://localhost:5000/tasks')
+      const data = await res.json()
+
+      console.log(data)
+    }
+    fetchTasks()
+  }, [])
 
   // Add Task
   const addTask = (task) => {
